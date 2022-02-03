c6a853268f92d5adf04285e3c33f0ad7e83e3783
Edgar Fernandez <tempurakohi@gmail.com>
2022-02-02 18:11:21 -0800

    made add the id from the HTML into a glocal variable in js, trying to get the rendering searches to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-// placemarker
+var APIKey = "6597c239dc318b2df8682f417cd75416"
+
+//DOM elements
+var cityInput= document.getElementById("cityInput")
+var searchButton= document.getElementById("searchButton")
+var searchHistory= document.getElementById("searchHistory")
+var clearButton= document.getElementById("clearButton")
+var weatherContainer= document.getElementById("weatherContainer")
+var weather= document.getElementById("weather")
+var forcast= document.getElementById("forcast")
+var searchStored= JSON.parse(localStorage.getItem("historyStored")) || []
+
+searchButton.addEventListener("click", function(){
+    const searchedCity= cityInput.value
+    searchStored.push(searchedCity)
+    localStorage.setItem("historyStored",JSON.stringify(searchStored))
+    console.log(searchStored)
+
+})
+
+function renderHistory(){
+    console.log(searchStored)
+}
