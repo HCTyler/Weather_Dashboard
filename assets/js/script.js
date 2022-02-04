50623a8f43af606df9079a3bdc270887c7626bfe
Edgar Fernandez <tempurakohi@gmail.com>
2022-02-04 13:26:23 -0800

    made button render search; needs to make it so it doesn't repeat previous search and make it so that seaarch goes through weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var APIKey = "6597c239dc318b2df8682f417cd75416"
+const APIKey = "6597c239dc318b2df8682f417cd75416"
 
 //DOM elements
 var cityInput= document.getElementById("cityInput")
@@ -8,16 +8,33 @@ var clearButton= document.getElementById("clearButton")
 var weatherContainer= document.getElementById("weatherContainer")
 var weather= document.getElementById("weather")
 var forcast= document.getElementById("forcast")
+var searchedCity=[]
 var searchStored= JSON.parse(localStorage.getItem("historyStored")) || []
 
+
+//taking searched items and storing it in local storage
 searchButton.addEventListener("click", function(){
-    const searchedCity= cityInput.value
+    searchedCity= cityInput.value
     searchStored.push(searchedCity)
+    console.log(searchStored,"Variable with empty Array, check filled")
     localStorage.setItem("historyStored",JSON.stringify(searchStored))
     console.log(searchStored)
-
+    renderHistory()
+})
+clearButton.addEventListener("click",function(){
+    localStorage.clear()
+    searchStored= []
+    renderHistory()
 })
 
 function renderHistory(){
-    console.log(searchStored)
+    console.log(searchedCity)
+    searchHistory.innerHTML= ""
+    for(var i=searchStored.length - 1; i>=0; i--){
+        const renderedHistory= document.createElement("button")
+        renderedHistory.setAttribute("type","text")
+        renderedHistory.setAttribute("class","form-control d-block bg-white")
+        renderedHistory.textContent= searchStored[i]
+        searchHistory.append(renderedHistory)
+    }
 }
