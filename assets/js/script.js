eef01705363e4e6efd3c56abb7fc6cce55e94e89
Edgar Fernandez <tempurakohi@gmail.com>
2022-02-08 05:05:22 -0800

    today weather rendering, forcast not rendering

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,12 @@ var cityInput= document.getElementById("cityInput")
 var searchButton= document.getElementById("searchButton")
 var searchHistory= document.getElementById("searchHistory")
 var clearButton= document.getElementById("clearButton")
-var form= document.getElementById("form")
+var formInput= document.getElementById("formInput")
 var weatherToday= document.getElementById("weatherToday")
 var forcast= document.getElementById("forcast")
 var cityTitle= document.getElementById("cityTitle")
-// var weatherPix= document.getElementById("weatherPix")
-// var temperature= document.getElementById("temperature")
-// var wind= document.getElementById("wind")
-// var humidity= document.getElementById("humidity") 
 var searchedCity=[]
+
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
@@ -25,15 +22,13 @@ function displayPast(){
     renderHistory()
 }
 function renderHistory(){
-    console.log(searchedCity)
     searchHistory.innerHTML= ""
 
-    for(var i=searchStored.length -1; i>=0; i--){
-        const renderedHistory= document.createElement("button")
+    for(var i=searchedCity.length -1; i>=0; i--){
+        var renderedHistory= document.createElement("button")
         renderedHistory.setAttribute("type","text")
         renderedHistory.setAttribute("aria-control","today forecast")
         renderedHistory.classList.add("history-btn", "btn-history")
-        renderedHistory.textContent= searchStored[i]
         renderedHistory.setAttribute("data-search", searchedCity[i])
         renderedHistory.textContent= searchedCity[i]
         searchHistory.append.apply(renderedHistory)
@@ -49,7 +44,7 @@ function appendSeach(search){
     renderHistory()
 }
 function renderToday(city, weather,timezone){
-    var date= dayjs().tz(timezone).formar("M/D/YYYY")
+    var date= dayjs().tz(timezone).format("M/D/YYYY")
 
     var tempF = weather.temp;
     var windMph = weather.wind_speed;
@@ -105,9 +100,127 @@ function renderToday(city, weather,timezone){
     weatherToday.innerHTML = '';
     weatherToday.append(card);
 }
+function renderCard(forecast,timezone){
+    var unixTs = forecast.dt;
+    var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+    var iconDescription = forecast.weather[0].description;
+    var tempF = forecast.temp.day;
+    var { humidity } = forecast;
+    var windMph = forecast.wind_speed;
+  
+    var col = document.createElement('div');
+    var card = document.createElement('div');
+    var cardBody = document.createElement('div');
+    var cardTitle = document.createElement('h5');
+    var weatherIcon = document.createElement('img');
+    var tempEl = document.createElement('p');
+    var windEl = document.createElement('p');
+    var humidityEl = document.createElement('p');
+  
+    col.append(card);
+    card.append(cardBody);
+    cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl);
+  
+    col.setAttribute('class', 'col-md');
+    col.classList.add('five-day-card');
+    card.setAttribute('class', 'card bg-primary h-100 text-white');
+    cardBody.setAttribute('class', 'card-body p-2');
+    cardTitle.setAttribute('class', 'card-title');
+    tempEl.setAttribute('class', 'card-text');
+    windEl.setAttribute('class', 'card-text');
+    humidityEl.setAttribute('class', 'card-text');
+  
 
-// displayPast()
-// searchHistory.addEventListener("click",clickHistory)
+    cardTitle.textContent = dayjs.unix(unixTs).tz(timezone).format('M/D/YYYY');
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', iconDescription);
+    tempEl.textContent = `Temp: ${tempF} °F`;
+    windEl.textContent = `Wind: ${windMph} MPH`;
+    humidityEl.textContent = `Humidity: ${humidity} %`;
+}
+function renderForecast(dailyForecast,timezone){
+    var startDate= dayjs().tz(timezone).add(1,"day").startOf("day").unix()
+    var endDate= dayjs().tz(timezone).add(6,"day").startOf("day").unix()
+
+    var forecastContainer= document.createElement("div")
+    var heading= document.createElement("h4")
+    forecastContainer.setAttribute("class","col-12")
+    heading.textContent= "5-Day Forecast"
+    forecastContainer.append(heading)
+
+    forcast.innerHTML= ""
+    forcast.append(forecastContainer)
+    for (var i= 0; i< dailyForecast.length; i++){
+        if(dailyForecast[i].dt >= startDate && dailyForecast[i]< endDate){
+            renderToday(dailyForecast[i],timezone)
+        }
+    }
+}
+function renderStuff(city,data){
+    renderToday(city, data.current, data.timezone)
+    renderCard(data.daily, data.timezone)
+}
+function getWeather(location){
+    var {lat}= location
+    var {lon}= location
+    var city= location.name
+    var weatherAPI= `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${APIKey}`
+
+    fetch(weatherAPI)
+        .then(function(res){
+            return res.json()
+        })
+        .then(function (data){
+            renderStuff(city, data)
+        })
+        .catch(function(err){
+            console.log(err)
+            console.log(city)
+        })
+}
+function getLocation(search){
+    var weatherAPI= `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=${APIKey}`
+
+    fetch(weatherAPI)
+        .then(function(res){
+            return res.json()
+        })
+        .then(function (data){
+            if (!data[0]) {
+                alert("Location not found")
+            } else {
+                appendSeach(search)
+                getWeather(data[0])
+            }
+        })
+        .catch(function(err){
+            console.error(err)
+        })
+}
+function formEvent(e) {
+    if(!cityInput.value){
+        return
+    }
+    e.preventDefault()
+    var search= cityInput.value.trim()
+    getLocation(search)
+    cityInput.value= ""
+}
+function clickHistory(e){
+    if(!e.target.matches(".btn-history")) {
+        return
+    }
+    var check= e.target
+    var search= check.getAttribute("data-search")
+    getLocation(search)
+}
+
+
+
+displayPast()
+formInput.addEventListener("submit",formEvent)
+searchHistory.addEventListener("click",clickHistory)
+
 // clearButton.addEventListener("click",function(){
 //     localStorage.clear()
 //     searchStored= []
